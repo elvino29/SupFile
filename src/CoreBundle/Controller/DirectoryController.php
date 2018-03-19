5bd6b1539f8d01ed619b389274bc374686acaa0c
elvino 29 <kevindoh26@gmail.com>
2018-03-19 21:44:35 +0100

    Start Webservice function

---
+++ src/CoreBundle/Controller/DirectoryController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KEVIN-PC
+ * Date: 19/03/2018
+ * Time: 17:41
+ */
+
+namespace CoreBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DirectoryController extends Controller
+{
+
+}
