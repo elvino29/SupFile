6c877babaf32eb909432b4c249ae0b8ed396e0ce
MASSENI <215401@supinfo.com>
2018-05-08 15:50:07 +0200

    download en cours OK

--- src/CoreBundle/Controller/FileController.php
+++ src/CoreBundle/Controller/FileController.php
@@ -10,19 +10,48 @@ namespace CoreBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use CoreBundle\Entity\User;
+use CoreBundle\Entity\File;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Filesystem\Exception\IOException;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 class FileController extends Controller
 {
-    public function indexAction(){
+   /* public function indexAction(){
 
     }
 
     public function getAllFileAction(){
 
-    }
+    }*/
 
-    public function downloadFileAction(){
+    // Télécharger des fichiers
+    /**
 
+     * @Rest\Get("/file/download/")
+     * @return JsonResponse
+     */
+    public function downloadFileAction(Request $request){
+
+        $user = $this->get("core_bundle.userprovider")
+            ->loadUserByToken($request->headers->get('authorization'));
+        if(!$user instanceof User) {
+            return $user;
+        }
+
+        //vérifier si le fichier existe
+
+        return new JsonResponse([
+
+                    $request->get('name'),
+                    $request->get('CreatedAt')
+        ]);
     }
 
 }
